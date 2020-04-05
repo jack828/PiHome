94f0a829623541b656460f19a978cf6eb50115a2
Jack Burgess <jackburgess124@gmail.com>
2020-04-05 23:43:05 +0100

    feat(nodes): Identify new, known, and unknown nodes

--- routes.js
+++ routes.js
@@ -9,23 +9,48 @@ const initRoutes = (serviceLocator, app) => {
     res.sendStatus(418)
   })
 
-  app.get('/node/identify(/)?(:identifier)?', async (req, res) => {
-    // Give node ids if not provided
-    let identifier = req.params.identifier || hat().slice(0, 4)
-    console.log(`[ NODE ]: Identifying node "${identifier}"`)
-    const node = await nodes.findOne({ id: identifier })
-    console.log(node)
+  app.get('/node/identify(/)?', async (req, res) => {
+    // cold start node - no saved identifier
+    const identifier = hat().slice(0, 4)
+    const insertOp = await nodes.insertOne({
+      id: identifier,
+      lastIdentified: new Date()
+    })
+    // welcome to the club
+    console.log('[ NODE ] new node', identifier, insertOp.insertedId)
+    res.status(200).end(identifier)
+  })
+
+  app.get('/node/identify/:identifier', async (req, res) => {
+    // two things can happen here:
+    //  - new node to the network
+    //  - known node re-identifying at boot
+    //
+    let identifier = req.params.identifier
+    console.log(`[ NODE ] identifying node "${identifier}"`)
+
+    // See if we find one
+    let node = await nodes.findOne({ id: identifier })
+    console.log('[ NODE ] read node', node)
+
     if (!node) {
+      // unknown node, welcome to the cluuuuuuuub
+      console.log('[ NODE ] new node', identifier)
       await nodes.insertOne({
         id: identifier,
         lastIdentified: new Date()
       })
     } else {
-      await nodes.findOneAndUpdate({ _id: node._id }, {
-        $set: { lastIdentified: new Date() }
-      })
+      console.log('[ NODE ] known node', identifier)
+      await nodes.findOneAndUpdate(
+        { _id: node._id },
+        {
+          $set: { lastIdentified: new Date() }
+        }
+      )
     }
 
+    console.log('[ NODE ] done identifying node', identifier)
     return res.status(200).end(identifier)
   })
 
