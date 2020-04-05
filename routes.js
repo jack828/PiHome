7da03b541f3b2d08816777e61185a6520f7ba8f1
Jack Burgess <jackburgess124@gmail.com>
2020-04-05 23:00:08 +0100

    feat: identifying a node + returns new identifier for nodes without one (first run)

--- routes.js
+++ routes.js
@@ -1,15 +1,32 @@
+const hat = require('hat')
+
 const initRoutes = (serviceLocator, app) => {
   const { serviceDatabase } = serviceLocator
+  const nodes = serviceDatabase.collection('node')
 
   app.get('/', (req, res) => {
     console.log(req.ip)
     res.sendStatus(418)
   })
 
-  app.get('/node/identify/:identifier', async (req, res) => {
-    const { identifier } = req.params
+  app.get('/node/identify(/)?(:identifier)?', async (req, res) => {
+    // Give node ids if not provided
+    let identifier = req.params.identifier || hat().slice(0, 4)
     console.log(`[ NODE ]: Identifying node "${identifier}"`)
-    res.sendStatus(418)
+    const node = await nodes.findOne({ id: identifier })
+    console.log(node)
+    if (!node) {
+      await nodes.insertOne({
+        id: identifier,
+        lastIdentified: new Date()
+      })
+    } else {
+      await nodes.findOneAndUpdate({ _id: node._id }, {
+        $set: { lastIdentified: new Date() }
+      })
+    }
+
+    return res.status(200).end(identifier)
   })
 
   app.get('/api/:collectionName/:from/:to', async (req, res) => {
