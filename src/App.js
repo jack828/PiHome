0566a0cc2d69db9744d5e5fe1f1c67ad0cf98caf
Jack Burgess <jackburgess124@gmail.com>
2020-04-11 02:48:46 +0100

    add pressure, light, and humidity charts

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ const App = () => {
   return (
     <Container style={{ margin: '24px' }}>
       <Chart title="Temperature" sensor="temperature" />
+      <Chart title="Pressure" sensor="pressure" />
+      <Chart title="Light" sensor="light" />
+      <Chart title="Humidity" sensor="humidity" />
     </Container>
   )
 }
