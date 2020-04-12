40925765a24fda118f918a9fe5cbe499ff474585
Jack Burgess <jackburgess124@gmail.com>
2020-04-12 23:53:51 +0100

    chart options: full width buttons

--- src/chart-options-panel.js
+++ src/chart-options-panel.js
@@ -41,11 +41,12 @@ export default ({ config, setConfig, reloadCharts }) => {
             <th className="col-1" scope="row">
               <FontAwesomeIcon icon="calendar-day" />
             </th>
-            <td className="col-4">Set date range</td>
-            <td className="col-7">
-              <ButtonGroup size="sm">
+            <td className="col-4">Date range</td>
+            <td className="col-7 d-flex">
+              <ButtonGroup size="sm" className="w-100">
                 {config.rangeOptions.map(option => (
                   <Button
+                    className="w-100"
                     key={option.name}
                     active={config.range.name === option.name}
                     onClick={() => setRangeOption(option)}
