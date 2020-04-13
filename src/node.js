6964138d8f462e70e77a8daaf70c343b86b2967d
Jack Burgess <jackburgess124@gmail.com>
2020-04-13 21:48:43 +0100

    refactor node i think it makes sense to split the editmode and non editmode structure

--- src/node.js
+++ src/node.js
@@ -57,62 +57,67 @@ const Node = ({
   const displayName = nickname || nodeId
   const [editedNickname, setEditedNickname] = useState(displayName)
   const [editedColour, setEditedColour] = useState(colour)
+  // TODO this makes more sense as embellished info
+  const statusIcon = getStatusIcon(lastReading)
 
   return (
     <tr className="d-flex text-center">
-      <th className="col-1" scope="row">
-        <FontAwesomeIcon icon={getStatusIcon(lastReading)} />
-      </th>
-      <td className="col-4">
-        {editMode ? (
-          <Input
-            value={editedNickname}
-            onChange={({ target: { value } }) => setEditedNickname(value)}
-          />
-        ) : (
-          displayName
-        )}
-      </td>
       {editMode ? (
-
-      <td
-        className="col-5">
-          <Input
-            value={editedColour}
-            onChange={({ target: { value } }) => setEditedColour(value)}
-          />
-      </td>
+        <>
+          <th className="col-1" scope="row">
+            <FontAwesomeIcon icon={statusIcon} />
+          </th>
+          <td className="col-4">
+            <Input
+              value={editedNickname}
+              onChange={({ target: { value } }) => setEditedNickname(value)}
+            />
+          </td>
+          <td className="col-5">
+            <Input
+              value={editedColour}
+              onChange={({ target: { value } }) => setEditedColour(value)}
+            />
+          </td>
+          <td className="col-1">
+            <FontAwesomeIcon icon="terminal" />
+          </td>
+          <td className="col-1">
+            <div
+              onClick={() => {
+                onChangeNickname(nodeId, editedNickname)
+                onChangeColour(nodeId, editedColour)
+                setEditMode(false)
+              }}
+            >
+              <FontAwesomeIcon icon="check" />
+            </div>
+          </td>
+        </>
       ) : (
-
-      <td
-        className="col-5"
-        onClick={() => setShowUptime(prevState => !prevState)}
-      >
-        {showUptime
-          ? formatDuration(new Date(), new Date(lastIdentified))
-          : formatRelative(new Date(lastReading), new Date())}
-      </td>
-        )}
-      <td className="col-1">
-        <FontAwesomeIcon icon="terminal" />
-      </td>
-      <td className="col-1">
-        {editMode ? (
-          <div
-            onClick={() => {
-              onChangeNickname(nodeId, editedNickname)
-              onChangeColour(nodeId, editedColour)
-              setEditMode(false)
-            }}
+        <>
+          <th className="col-1" scope="row">
+            <FontAwesomeIcon icon={statusIcon} />
+          </th>
+          <td className="col-4">displayName</td>
+          <td
+            className="col-5"
+            onClick={() => setShowUptime(prevState => !prevState)}
           >
-            <FontAwesomeIcon icon="check" />
-          </div>
-        ) : (
-          <div onClick={() => setEditMode(true)}>
-            <FontAwesomeIcon icon="edit" />
-          </div>
-        )}
-      </td>
+            {showUptime
+              ? formatDuration(new Date(), new Date(lastIdentified))
+              : formatRelative(new Date(lastReading), new Date())}
+          </td>
+          <td className="col-1">
+            <FontAwesomeIcon icon="terminal" />
+          </td>
+          <td className="col-1">
+            <div onClick={() => setEditMode(true)}>
+              <FontAwesomeIcon icon="edit" />
+            </div>
+          </td>
+        </>
+      )}
     </tr>
   )
 }
