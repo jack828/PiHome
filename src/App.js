d8c55631e61f5ddfa572b2b1a21e742f9af76bf2
Jack Burgess <jackburgess124@gmail.com>
2020-04-17 20:58:58 +0100

    move all charts to connected version

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Container, Col, Row } from 'reactstrap'
 import sub from 'date-fns/sub'
-import Chart from './chart'
+import Chart from './chart/'
 import NodePanel from './node-panel'
 import ChartOptionsPanel from './chart-options-panel'
 
@@ -49,27 +49,10 @@ const defaultChartConfig = () => {
 }
 
 const App = () => {
-  const [charts, setCharts] = useState(null)
   const [nodes, setNodes] = useState(null)
   const [chartConfig, setChartConfig] = useState(defaultChartConfig())
-  const loadCharts = async rangeOption => {
-    const sensors = ['temperature', 'pressure', 'light', 'humidity']
-    const { start, end } = rangeOption || chartConfig.range
-
-    const sensorData = await Promise.all(
-      sensors.map(sensor => api(`/api/sensor/${sensor}/${start}/${end}/`))
-    )
-    const chartData = sensors.reduce(
-      (data, sensor) => ({
-        ...data,
-        [sensor]: sensorData[sensors.indexOf(sensor)]
-      }),
-      {}
-    )
-    setCharts(chartData)
-  }
   const loadNodes = async () => setNodes(await api(`/api/nodes`))
-  const reloadData = () => Promise.all([loadNodes(), loadCharts()])
+  const reloadData = () => Promise.all([loadNodes()])
 
   const handleSaveNode = async data => {
     console.log('updating node', data)
@@ -93,51 +76,35 @@ const App = () => {
     <Container fluid>
       <Row>
         <Col xs={{ size: 12, order: 1 }} lg={{ size: 9, order: 0 }}>
-          {charts && (
-            <>
-              <Row>
-                <Col xs="12" md="6">
-                  <Chart
-                    title="Temperature"
-                    sensor="temperature"
-                    yMin={20}
-                    yMax={30}
-                    data={charts.temperature}
-                    config={chartConfig}
-                  />
-                </Col>
-                <Col xs="12" md="6">
-                  <Chart
-                    title="Pressure"
-                    sensor="pressure"
-                    data={charts.pressure}
-                    config={chartConfig}
-                  />
-                </Col>
-              </Row>
+          <Row>
+            <Col xs="12" md="6">
+              <Chart
+                title="Temperature"
+                sensor="temperature"
+                yMin={20}
+                yMax={30}
+                config={chartConfig}
+              />
+            </Col>
+            <Col xs="12" md="6">
+              <Chart title="Pressure" sensor="pressure" config={chartConfig} />
+            </Col>
+          </Row>
 
-              <Row>
-                <Col xs="12" md="6">
-                  <Chart
-                    title="Light"
-                    sensor="light"
-                    data={charts.light}
-                    config={chartConfig}
-                  />
-                </Col>
-                <Col xs="12" md="6">
-                  <Chart
-                    title="Humidity"
-                    sensor="humidity"
-                    yMin={0}
-                    yMax={100}
-                    data={charts.humidity}
-                    config={chartConfig}
-                  />
-                </Col>
-              </Row>
-            </>
-          )}
+          <Row>
+            <Col xs="12" md="6">
+              <Chart title="Light" sensor="light" config={chartConfig} />
+            </Col>
+            <Col xs="12" md="6">
+              <Chart
+                title="Humidity"
+                sensor="humidity"
+                yMin={0}
+                yMax={100}
+                config={chartConfig}
+              />
+            </Col>
+          </Row>
         </Col>
         <Col xs={{ size: 12, order: 0 }} lg={{ size: 3, order: 1 }}>
           <Row>
@@ -150,7 +117,9 @@ const App = () => {
                   <ChartOptionsPanel
                     config={chartConfig}
                     setConfig={setChartConfig}
-                    reloadCharts={loadCharts}
+                    reloadCharts={() => {
+                      /* TODO */
+                    }}
                   />
                 </Col>
               </>
